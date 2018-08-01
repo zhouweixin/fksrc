216b65f9801f8fea714dc4cdf4f1309bd1ac92f4
zhouweixin <1216840597@qq.com>
2018-08-01 13:34:10 +0800

    初始导入

---
+++ src/main/java/com/hnu/fk/FkApplication.java
@@ -0,0 +1,12 @@
+package com.hnu.fk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FkApplication.class, args);
+    }
+}
